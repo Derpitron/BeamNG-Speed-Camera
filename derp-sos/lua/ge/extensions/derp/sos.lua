040757d9e072fbff46c28c79e848685d22e1397e
Derpitron <priya.aravamuthan@protonmail.com>
2022-12-16 23:53:01 +0530

    feat(Speed Distance): Attempt to implement Speed Distance Function
    UNTESTED

--- derp-sos/lua/ge/extensions/derp/sos.lua
+++ derp-sos/lua/ge/extensions/derp/sos.lua
@@ -3,9 +3,42 @@
 -- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
 
 -- ORBIT CAMERA
+-- Author: Derpitron
+
+-- local sensors = require("sensors")
 
 local M = {}
+M.__index = M
 
 print("theRealTest200")
+
+local calculatedCamPos = vec3()
+local camPos = vec3()
+local targetPos = vec3()
+local ref = vec3()
+
+--todo: function to increase distance based on speed
+function M:update(data)
+  --Set the speedDist(ance) to be 0.001 metres * velocity limited to 150 m/s, with the effect maxxing out at 130 m/s
+  local speedDist = 0.001 * math.min(data.vel:length()/150, 0.85)
+  local dist = self.camDist
+
+	calculatedCamPos:setScaled(dist + speedDist)
+
+  camPos:set(push3(calculatedCamPos) + targetPos + self.orbitOffset)
+  targetPos = data.veh:getBBCenter() - (data.pos + ref)
+  self.camLastPos:set(camPos)
+
+  self.camLastTargetPos:set(targetPos)
+  self.camVel:set((push3(camPos) - self.camLastPos) / data.dt)
+  self.camLastPos:set(camPos)
+  self.camLastDist = dist
+
+  -- application
+  data.res.pos = vec3(camPos)
+  data.res.targetPos:set(targetPos)
+
+end
+--todo: function to increase or decrease distance based on g force
 
 return M
