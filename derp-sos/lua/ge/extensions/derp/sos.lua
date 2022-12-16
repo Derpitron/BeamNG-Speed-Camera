9304d531f1065a79cf66b6dea124467d161079d9
Derpitron <priya.aravamuthan@protonmail.com>
2022-12-16 16:58:02 +0530

    feat: Got print() working

---
+++ derp-sos/lua/ge/extensions/derp/sos.lua
@@ -0,0 +1,11 @@
+-- This Source Code Form is subject to the terms of the bCDDL, v. 1.1.
+-- If a copy of the bCDDL was not distributed with this
+-- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
+
+-- ORBIT CAMERA
+
+local M = {}
+
+print("theRealTest200")
+
+return M
