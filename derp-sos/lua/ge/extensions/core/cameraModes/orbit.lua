54e75bc1fceea83409a059e22681cd99d4ae790d
Derpitron <priya.aravamuthan@protonmail.com>
2023-06-06 15:28:06 +0530

    gonna overwrite orbit cam a bit by adding some utility functions xd

--- derp-sos/lua/ge/extensions/core/cameraModes/orbit.lua
+++ derp-sos/lua/ge/extensions/core/cameraModes/orbit.lua
@@ -345,7 +345,7 @@ function C:update(data)
   local refToRear = rear:distance(targetPos)
   local hdegToRad = math.pi/180 * 0.5
 
-  local fov = 65 --self.fov
+  local fov = self.fov
   local fovdistDiff = 0
   local fovModifier = self.skipFovModifier and 0 or self.fovModifier
 
@@ -391,7 +391,8 @@ function C:update(data)
   data.res.fov = fov
   data.res.targetPos:set(targetPos)
   self.collision:update(data)
-  dump(data)
+
+  print("loadedorbitfunni")
   return true
 end
 
